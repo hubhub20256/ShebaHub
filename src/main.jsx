900a8698a80d8b6a46300e4b34384a1e68e4346c
GuyC21 <e4guycohen@outlook.com>
2025-12-12 17:04:26 +0200

    fixed app.jsx and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx"; // We now import the App we just defined
 import "./index.css";
-import {
-  Home,
-  Search,
-  Research,
-  Profile,
-  About,
-  Mentors,
-  Researches,
-  Apprentices,
-} from "./pages";
-
-import Layout from "./components/Layout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          {/* These routes use parameters (e.g., /user/1) */}
-          <Route path="/user/:id" element={<Profile />} />
-          <Route path="/research/:id" element={<Research />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/Apprentices" element={<Apprentices />} />
-          <Route path="/Mentors" element={<Mentors />} />
-          <Route path="/Researches" element={<Researches />} />
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
