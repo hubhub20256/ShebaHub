33adf7bdf571374b34b019a7c922b600d8cc49a8
GuyC21 <e4guycohen@outlook.com>
2025-12-12 17:02:48 +0200

    fixed imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home.jsx";
-import Search from "./pages/Search.jsx";
-import Research from "./pages/Research.jsx";
-import Profile from "./pages/Profile.jsx";
-import About from "./pages/About.jsx";
-import Mentors from "./pages/Mentors.jsx";
-import Researches from "./pages/Researches.jsx";
+import {
+  Home,
+  Search,
+  Research,
+  Profile,
+  About,
+  Mentors,
+  Researches,
+  Apprentices,
+} from "./pages";
 
 import Layout from "./components/Layout.jsx";
-import Apprentices from "./pages/Apprentices.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
