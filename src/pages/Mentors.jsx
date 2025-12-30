4ba4cebb8c1976dbfb54dc9c6cfdd8a353511662
lorenkr <lorenkr20@gmail.com>
2025-12-30 22:52:17 +0200

    Add names, adjust appearance and configure layout to 2 cards per row

--- src/pages/Mentors.jsx
+++ src/pages/Mentors.jsx
@@ -6,27 +6,30 @@ import image3 from '../assets/mentor3.jpg';
 
 const mockMentors = [
   {
+    name: "דנה כהן",
     id: 1,
     gender: "נקבה",
     email: "dana.k@med-example.com",
     specialty: "רפואת ילדים והתפתחות הילד",
     degrees: "דוקטור לרפואה (MD)",
     Educational_institution: "אוניברסיטת תל אביב - הפקולטה לרפואה",
-    profileImage: image1
+    profileImage: image2
   },
   {
+    name: "יותם לוי",
     id: 2,
     gender: "זכר",
     email: "yotam.lev@hospital-demo.co.il",
     specialty: "כירורגיה לב-חזה וצנתורים",
     degrees: "דוקטור לרפואה (MD) ו-PhD בפיזיולוגיה",
     Educational_institution: "האוניברסיטה העברית והדסה עין כרם",
-    profileImage: image2
+    profileImage: image1
   },
   {
+    name: "מיכאל לוי",
     id: 3,
-    gender: "נקבה",
-    email: "michal.s@clinic-test.org",
+    gender: "זכר",
+    email: "michael.s@clinic-test.org",
     specialty: "נוירולוגיה קלינית",
     degrees: "דוקטור לרפואה (MD) בהצטיינות יתרה",
     Educational_institution: "אוניברסיטת בן-גוריון בנגב",
@@ -34,19 +37,14 @@ const mockMentors = [
   },
 ];
 
+
+
 export default function Mentors() {
   return (
-    <div style={{ padding: 24 }} dir="rtl">
-      <h1 style={{ marginBottom: 12 }}>מנחים</h1>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(260px, 1fr))",
-          gap: 14,
-        }}
-      >
-        {mockMentors.map((m) => (
+    <div className="page-wrapper" dir="rtl">
+     <h1 className="main-title">מנחים</h1>
+      <div className="cards-grid">
+        {mockMentors.slice(0, 20).map((m) => (
           <MentorCard key={m.id} mentor={m} />
         ))}
       </div>
