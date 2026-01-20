62a193625fef75585b2d0f4b096f3b3fb738b6b0
bettyhv1 <betty.hv1@gmail.com>
2026-01-20 23:09:06 +0200

    Updated images and fixed the logout link

--- ShebaHub_Front/src/pages/Home.jsx
+++ ShebaHub_Front/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <main className="home-page">
       <section className="home-hero">
         <div className="home-left" dir="rtl">
-          <h1 className="home-title">ברוכה הבאה לShebaHub</h1>
+          <h1 className="home-title"> ברוכים הבאים ל-hebaHub</h1>
 
           <p className="home-subtitle">
             פלטפורמה לחיבור בין חוקרים לסטודנטים ומתמחים לקידום מחקר, חדשנות
