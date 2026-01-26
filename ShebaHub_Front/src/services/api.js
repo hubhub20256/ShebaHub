477c68e15fa3d74c8902d31ab57ff186146a214e
lorenkr <lorenkr20@gmail.com>
2026-01-26 21:26:09 +0200

    added /api to server url

--- ShebaHub_Front/src/services/api.js
+++ ShebaHub_Front/src/services/api.js
@@ -1,5 +1,5 @@
 // API Service for Backend Communication
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'http://shebahub.hitheal.org.il:8085/api';
 
 // ---------------- Token helpers ----------------
 const decodeJwtPayload = (token) => {
