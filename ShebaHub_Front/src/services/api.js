850bbc68a9a31e6d9679077644433188d6582674
GuyC21 <e4guycohen@outlook.com>
2026-02-15 19:03:20 +0200

    s was added to api rout

--- ShebaHub_Front/src/services/api.js
+++ ShebaHub_Front/src/services/api.js
@@ -1,5 +1,5 @@
 // API Service for Backend Communication
-const API_BASE_URL = 'http://shebahub.hitheal.org.il:8085/api';
+const API_BASE_URL = 'https://shebahub.hitheal.org.il:8085/api';
 
 // ---------------- Token helpers ----------------
 const decodeJwtPayload = (token) => {
