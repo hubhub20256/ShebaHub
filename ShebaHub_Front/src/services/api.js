13dd1bfdcab2bcf791e050d9dcad74753de0810a
GuyC21 <e4guycohen@outlook.com>
2026-03-09 16:26:18 +0200

    updated the api address

--- ShebaHub_Front/src/services/api.js
+++ ShebaHub_Front/src/services/api.js
@@ -1,6 +1,5 @@
 // API Service for Backend Communication
-//export const API_BASE_URL = 'https://shebahub.hitheal.org.il:8085/api';
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'https://shebahub.hitheal.org.il:8085/api';
 
 // ---------------- Token helpers ----------------
 const decodeJwtPayload = (token) => {
